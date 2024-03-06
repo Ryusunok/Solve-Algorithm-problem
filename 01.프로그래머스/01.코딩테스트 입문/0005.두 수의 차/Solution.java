c858e549a2b88ddaeb77c420876b4e2d5eaf053d
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-03-06 17:34:17 +0900

    Create Solution.java

---
+++ 01.프로그래머스/01.코딩테스트 입문/0005.두 수의 차/Solution.java
@@ -0,0 +1,12 @@
+[주소]
+https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+[문제 설명]
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+[풀이]
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
