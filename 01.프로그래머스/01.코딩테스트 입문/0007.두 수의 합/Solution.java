c5e071873a3da0f4da8d8a78c97e67de138847ca
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-03-06 17:37:49 +0900

    Create Solution.java

---
+++ 01.프로그래머스/01.코딩테스트 입문/0007.두 수의 합/Solution.java
@@ -0,0 +1,12 @@
+[주소]
+https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+[문제 설명]
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+[풀이]
+  class Solution {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
