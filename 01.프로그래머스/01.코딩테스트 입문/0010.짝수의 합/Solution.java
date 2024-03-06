1908e8d69aedd10d8c2ba86f7f029d2283636b4d
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-03-06 17:55:55 +0900

    Create Solution.java

---
+++ 01.프로그래머스/01.코딩테스트 입문/0010.짝수의 합/Solution.java
@@ -0,0 +1,16 @@
+  [주소]
+  https://school.programmers.co.kr/learn/courses/30/lessons/120831
+
+  [문제 설명]
+  정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+    
+  [풀이]
+  class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i+=2) {
+            answer += i;
+        }
+        return answer;
+    }
+}
