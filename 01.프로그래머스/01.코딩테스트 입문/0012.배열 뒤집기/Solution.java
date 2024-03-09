1ee55c42e6bfbf3677c661645a361330d7e5728a
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-03-09 13:38:29 +0900

    Create Solution.java

---
+++ 01.프로그래머스/01.코딩테스트 입문/0012.배열 뒤집기/Solution.java
@@ -0,0 +1,17 @@
+[주소]
+https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+[문제 설명]
+정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+[풀이]
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for(int i = num_list.length - 1; i >= 0; i--) {
+            answer[num_list.length - 1 - i] = num_list[i];
+        }
+        return answer;
+    }
+}
