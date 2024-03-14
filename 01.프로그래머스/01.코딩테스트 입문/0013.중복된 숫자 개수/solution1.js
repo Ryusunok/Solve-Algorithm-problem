eff6aedb7d13868dfa3bc608fdf17f1cf7410a37
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-03-14 18:45:20 +0900

    Create solution1.js

---
+++ 01.프로그래머스/01.코딩테스트 입문/0013.중복된 숫자 개수/solution1.js
@@ -0,0 +1,9 @@
+function solution1(array, n) {
+    let count = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === n) {
+            count++;
+        }
+    }
+    return count;
+}
